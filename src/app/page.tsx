5f12e68073b31c828ff440dbad6101a4ae90a5e9
0xBlackStack - OWNER <0xblackstack@gmail.com>
2025-10-29 19:49:47 +0530

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div>
-      <Button variant={"default"}>
-        Click me!
-      </Button>
+      <h1>
+        Aura!
+      </h1>
     </div>
   );
 }
