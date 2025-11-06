9a9d16b025452f35a142dc6843449863ebec86c2
0xBlackStack - OWNER <0xblackstack@gmail.com>
2025-11-06 20:40:28 +0530

    11: Project Header

--- src/app/projects/ui/views/project-view.tsx
+++ src/app/projects/ui/views/project-view.tsx
@@ -2,15 +2,22 @@
 
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { MessageContainer } from "../components/message-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma/client";
+import { ProjectHeader } from "../components/project-header";
 
 export const ProjectView = ({ projectId }: { projectId: string }) => {
+    const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+
     return (
         <div className="h-screen">
             <ResizablePanelGroup direction="horizontal">
                 <ResizablePanel defaultSize={20} minSize={20} className="flex flex-col min-h-0">
+                    <Suspense fallback={<p>Loading Project...</p>}>
+                        <ProjectHeader projectId={projectId} />
+                    </Suspense>
                     <Suspense fallback={<p>Loading messages...</p>}>
-                        <MessageContainer projectId={projectId} />
+                        <MessageContainer projectId={projectId} activeFragment={activeFragment} setActiveFragment={setActiveFragment} />
                     </Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
